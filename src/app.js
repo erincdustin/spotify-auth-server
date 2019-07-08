d19a749697c0b3d09989d591982a4340ca89f648
Erin Dustin <erincdustin@gmail.com>
2019-07-08 08:58:10 -0400

    prepped for deployment

--- src/app.js
+++ src/app.js
@@ -4,10 +4,6 @@ require('dotenv').config();
 const express = require('express');
 let request = require('request');
 let querystring = require('querystring');
-// const morgan = require('morgan');
-// const cors = require('cors');
-// const helmet = require('helmet');
-// const { NODE_ENV } = require('./config');
 
 const app = express();
 
@@ -47,28 +43,5 @@ app.get('/callback', function(req, res) {
     res.redirect(uri + '?access_token=' + access_token);
   });
 });
-
-// const morganOption = (NODE_ENV === 'production')
-//   ? 'tiny'
-//   : 'common';
-
-// app.use(morgan(morganOption));
-// app.use(cors());
-// app.use(helmet());
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, world');
-// });
-
-// app.use(function errorHandler(error, req, res, next) {
-//   let response;
-//   if (NODE_ENV === 'production') {
-//     response = { error: { message: 'server error'} };
-//   } else {
-//     console.error(error);
-//     response = { message: error.message, error };
-//   }
-//   res.status(500).json(response);
-// });
 
 module.exports = app;
