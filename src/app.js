e1326273cc197930f71756ce51881552afb1d4b7
Erin Dustin <erincdustin@gmail.com>
2019-07-10 09:13:34 -0700

    changed endpoint

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.get('/callback', function(req, res) {
   };
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token;
-    let uri = process.env.FRONTEND_URI || 'http://localhost:3000/redirect';
+    let uri = process.env.FRONTEND_URI;
     res.redirect(uri + '?access_token=' + access_token);
   });
 });
